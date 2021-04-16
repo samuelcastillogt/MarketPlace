46f6980f4d929625b6023e52ca4fa8eb853fa6f6
samuel castillo <samuelcastilloseo@gmail.com>
2021-04-15 18:22:09 -0600

    agrege la renderizacion de los perfiles

--- index.js
+++ index.js
@@ -16,27 +16,25 @@ app.listen(3000, ()=>{
 app.get("/", (req, res)=>{
   res.render("index")
 })
-app.get("/hhh", (req, res)=>{
-  res.render("perfil")
-})
-  // app.get("/:id",  function(req, res){
-  //   const { id }= req.params
-  //   res.render("perfil")
-  // //   var firebase = require("firebase/app");
-  // //   var db = firebase.firestore()
-  // //   var busicess =db.collection("bisiness");
-  // //   let cityRef = busicess.doc(id);
+
+  app.get("/:id",  function(req, res){
+    const { id }= req.params
+    
+    var firebase = require("firebase/app");
+    var db = firebase.firestore()
+    var busicess =db.collection("bisiness");
+    let cityRef = busicess.doc(id);
   
-  // //   let getDoc = cityRef.get()
-  // //     .then(doc => {
-  // //       if (!doc.exists) { 
-  // //         return 
-  // //       } else {
-  // //         res.render("perfil", doc.data())
-  // //       }
-  // //     })
-  // //     .catch(err => {
-  // //       console.log('Error getting document', err);
-  // //     });
-  //  })
+    let getDoc = cityRef.get()
+      .then(doc => {
+        if (!doc.exists) { 
+          return 
+        } else {
+          res.render("perfil", doc.data())
+        }
+      })
+      .catch(err => {
+        console.log('Error getting document', err);
+      });
+   })
 // require("./routes")(app)
