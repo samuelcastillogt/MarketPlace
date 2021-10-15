fff27b6a4c8b14bd9262be3a39e24c97121847f0
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2021-10-14 19:14:19 -0600

    Cambio de Estilo y verificacion del login

--- index.js
+++ index.js
@@ -1,69 +1,150 @@
 const express = require("express")
+const ejs = require("ejs")
 const path = require('path');
+const serverless = require('serverless-http');
 const app = express()
-/////firebase
-require("./db/db.js")
-///Mildaware
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public'), {
-    etag: false,
-  }));
-  
-app.listen(3000, ()=>{
-    console.log("hola")
+app.use(express.static(path.join(__dirname, 'static')));
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+require("./modules/db.js")
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore()
+const anuncios = ()=> db.collection("posts").get()
+const post = ()=> db.collection("entradas").get()
+const entrada = (id)=> db.collection("entradas").doc(id).get()
+app.listen(80, ()=>{
+  console.log("hola")
 })
-app.get("/", async(req, res)=>{
-  var firebase = require("firebase/app");
-    var db = firebase.firestore()
-    var business = await db.collection("bisiness").get()
-    
-    var { docs }= business
-    const negocios = docs.map(negocio =>({datos: negocio.data()}))
-    var blog = await db.collection("posts").get()
-    var { docs } = blog
-    const posts = docs.map(post =>({datos: post.data()}))
-    var datos = {negocios: negocios, posts: posts}
-   res.render("index", { datos })
+app.get("/", async(req, res)=>{  
+    const peticion =  await anuncios()
+    const { docs } = peticion
+    const ads = docs.map(entrada =>({ adid: entrada.id, ad: entrada.data()}))
+    res.render("index", { ads } )
 })
+app.get("/blog", async(req, res)=>{  
+  const peticion =  await post()
+  const { docs } = peticion
+  const ads = docs.map(entrada =>({ adid: entrada.id, ad: entrada.data()}))
+  res.render("blogger", { ads } )
+})
+app.get("/blog/:id",  async(req, res)=>{
+  const { id }= req.params
+  let cityRef = await entrada(id)
+  const  datos  = cityRef.data()
+  console.log(datos)
+  // let peticion = await busicess.where("categoria", "==", entrada.categoria).get()
+  // const { docs } = peticion
+  //  const entradas = docs.map(entrada =>({ datos: entrada.data()}))
+  res.render("post", datos)
+ })
 
-  app.get("/business/:id",  async(req, res)=>{
+app.get("/anuncio/:id",  async(req, res)=>{
     const { id }= req.params
-    
-    var firebase = require("firebase/app");
-    var db = firebase.firestore()
-    var busicess =db.collection("bisiness");
-    let cityRef = await busicess.doc(id).get();
-    let cityRef2 = await busicess.doc(id).collection("publicaciones").get();
+    var busicess =db.collection("posts");
+    let cityRef = await busicess.doc(id).get()
+    const  entrada  = cityRef.data()
+    let autor = await db.collection("autor").doc(entrada.autor).get()
+     const anuncio = {datos: entrada, autor: autor.data()}
+     busicess.doc(id).update({visitas: entrada.visitas + 1})
+    res.render("blog", anuncio)
+   })
 
-    const  negocio  = cityRef.data()
-    var { docs } = cityRef2
-    const posts = docs.map(publicacion =>({ publicacion: publicacion.data()}))
-    let peticion = await busicess.where("categoria", "==", negocio.categoria).get()
-    var { docs } = peticion
-     const negocios = docs.map(negocio =>({ datos: negocio.data()}))
-    const perfil = {datos: negocio, relaciondas: negocios, publicacion: posts}
-    res.render("perfil", perfil)
-   
+   app.get("/tienda/:id",  async(req, res)=>{
+    const { id }= req.params
+    const db = firebase.firestore()
+    const autor = await db.collection("autor").doc(id).get()
+    const peticion =  await db.collection("posts").where("autor", "==", id ).get()
+    const { docs } = peticion
+    const ads = docs.map(ad =>({ id: ad.id, ad: ad.data()}))
+
+    const datos ={ ads: ads, autor: autor.data(), id: id}
+    
+    res.render("tienda", {datos})
    })
-   app.get("/blog/:slug",  function(req, res){
-    const { slug }= req.params
-      
+   app.post("/tienda/calificar/:id", (req, res)=>{
+    const { id }= req.params
+    const db = firebase.firestore()
+    const datos = db.collection("posts").doc(id)
+
+   })
+   app.get("/categorias",  function(req, res){
+    const { id }= req.params
     var firebase = require("firebase/app");
-    var db = firebase.firestore()
-    var blog =db.collection("posts");
-    let cityRef = blog.doc(slug);
-    let getDoc = cityRef.get()
-      .then(doc => {
-        if (!doc.exists) { 
-          return 
-        } else {
-          res.render("blog", doc.data())
-        }
-      })
-      .catch(err => {
-        console.log('Error getting document', err);
-      });
-   
+    res.render("categorias")
     
    })
+   app.get("/inscribete",  function(req, res){
+    res.render("landing1")
+    
+   })
+   app.get("/categorias/:id",  async(req, res)=>{
+    const { id }= req.params
+    const db = firebase.firestore()
+    const peticion =  await db.collection("posts").where("categoria", "==", id ).get()
+    const { docs } = peticion
+  
+    const ads = docs.map(entrada =>({ adid: entrada.id, ad: entrada.data()}))
+    console.log(ads)
+    const datos ={ ads: ads}
+    
+    res.render("categoria", { ads })
+   })
+   app.get("/app",  async(req, res)=>{
+      res.render("app")
+    
+   })
+   app.get("/app/:id", async(req, res)=>{
+    const { id }= req.params
+    const db = firebase.firestore()
+    const peticion =  await db.collection("posts").where("autor", "==", id ).get()
+    const { docs } = peticion
+    const ads = docs.map(ad =>({ id: ad.id, ad: ad.data()}))
+    res.render("dashbord", { ads })
+   })
+   app.get("/borrar/:id", async(req, res)=>{
+    const db = firebase.firestore()
+    const { id }= req.params
+    const datos= await db.collection("posts").doc(id).get()
+    const doc = {datos: datos.data(), id: datos.id}
+    res.render("dash", {doc})
+    
+   })
+   app.get("/app/delete/:id", async(req, res)=>{
+    const db = firebase.firestore()
+    const { id }= req.params
+
+    await db.collection("posts").doc(id).delete()
+    
+    res.redirect("/app")
+   })
+   app.get("/app/edit/:id", async(req, res)=>{
+    const db = firebase.firestore()
+    const { id }= req.params
+    const peticion= await db.collection("posts").doc(id).get()
+    const  datos  = peticion.data()
+    res.render("editar", peticion.data())
+   })
+   app.get("/app/crear",  function(req, res){
+    const { id }= req.params
+    var firebase = require("firebase/app");
+    res.render("crear")
+      
+   })
+   
+   app.get("/api/home", async(req, res)=>{
+    const db = firebase.firestore()
+    const peticion =  await db.collection("posts").get()
+    const { docs } = peticion
+    const ads = docs.map(entrada =>({ adid: entrada.id, ad: entrada.data()}))
+    res.render("app-home", { ads })
+   })
+   app.get("/politicas/cokkies", (req, res)=>{
+     res.render("cokkis")
+   })
+   app.get("/sitemaps/sitemap", (req, res)=>{
+    res.sendFile(__dirname + "/static/sitemaps/sitemap.xml")
+  })
+ 
